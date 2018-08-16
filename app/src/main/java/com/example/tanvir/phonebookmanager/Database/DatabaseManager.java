93aa13d634accc382e35bd3ee6994e4fec6ba4fb
saiful00000 <>
2018-08-17 03:32:46 +0600

    crude options added

--- app/src/main/java/com/example/tanvir/phonebookmanager/Database/DatabaseManager.java
+++ app/src/main/java/com/example/tanvir/phonebookmanager/Database/DatabaseManager.java
@@ -24,13 +24,35 @@ public class DatabaseManager {
         contentValues.put(DatabaseHelper.CONTACT_NAME,contactsInfo.getContactName());
         contentValues.put(DatabaseHelper.CONTACT_NUMBER,contactsInfo.getContactNumber());
         contentValues.put(DatabaseHelper.CONTACT_EMAIL,contactsInfo.getContactEmail());
-        contentValues.put(DatabaseHelper.CONTACT_EMAIL,contactsInfo.getContactEmail());
+        contentValues.put(DatabaseHelper.CONTACT_DESCRIPTION,contactsInfo.getContactDescription());
         long insertedRow=sqLiteDatabase.insert(DatabaseHelper.CONTACT_TABLE,null,contentValues);
         sqLiteDatabase.close();
         return insertedRow;
     }
 
-    public ArrayList<ContactsInfo> getAllStudent(){
+
+    public long updateContactInfo(ContactsInfo contactsInfo){
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(DatabaseHelper.CONTACT_NAME,contactsInfo.getContactName());
+        contentValues.put(DatabaseHelper.CONTACT_NUMBER,contactsInfo.getContactNumber());
+        contentValues.put(DatabaseHelper.CONTACT_EMAIL,contactsInfo.getContactEmail());
+        contentValues.put(DatabaseHelper.CONTACT_DESCRIPTION,contactsInfo.getContactDescription());
+        long updatedRow = sqLiteDatabase.update(DatabaseHelper.CONTACT_TABLE,contentValues,
+                DatabaseHelper.CONTACT_ID+" =? ",new String[]{String.valueOf(contactsInfo.getId())});
+        return updatedRow;
+    }
+
+    public long deleteStudent(int id){
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+        long deletedRow = sqLiteDatabase.delete(DatabaseHelper.CONTACT_TABLE,DatabaseHelper.CONTACT_ID+" =? ",
+                new String[]{String.valueOf(id)});
+        sqLiteDatabase.close();
+        return deletedRow;
+    }
+
+
+    public ArrayList<ContactsInfo> getAllContacts(){
         SQLiteDatabase sqLiteDatabase=databaseHelper.getReadableDatabase();
         ArrayList<ContactsInfo>contacts=new ArrayList<>();
         String selectQuery="select * from "+DatabaseHelper.CONTACT_TABLE;
@@ -47,6 +69,22 @@ public class DatabaseManager {
             }while(cursor.moveToNext());
         }
         return contacts;
+    }
+
+    public ContactsInfo getContactsById(int id){
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getReadableDatabase();
+        String selectQuery="select * from "+DatabaseHelper.CONTACT_TABLE
+                +" where id = "+id;
+        Cursor cursor = sqLiteDatabase.rawQuery(selectQuery,null);
+        ContactsInfo contactsInfo = null;
+        if(cursor.moveToFirst()){
+            String contactName = cursor.getString(cursor.getColumnIndex(DatabaseHelper.CONTACT_NAME));
+            String contactNumber = cursor.getString(cursor.getColumnIndex(DatabaseHelper.CONTACT_NUMBER));
+            String contactEmail = cursor.getString(cursor.getColumnIndex(DatabaseHelper.CONTACT_EMAIL));
+            String contactDescription = cursor.getString(cursor.getColumnIndex(DatabaseHelper.CONTACT_DESCRIPTION));
+            contactsInfo =new ContactsInfo(id,contactName,contactNumber,contactEmail,contactDescription);
+        }
+        return contactsInfo;
     }
 
 
