56037bcd799dffdea71315c8d01c0d26b84af246
hatanvir <tanvirhasan552@gmail.com>
2018-08-28 00:17:55 +0600

    Search feature and favorite feature added

--- app/src/main/java/com/example/tanvir/phonebookmanager/Fragments/FavoriteFragment.java
+++ app/src/main/java/com/example/tanvir/phonebookmanager/Fragments/FavoriteFragment.java
@@ -1,16 +1,34 @@
 package com.example.tanvir.phonebookmanager.Fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.tanvir.phonebookmanager.Activity.MainActivity;
+import com.example.tanvir.phonebookmanager.Activity.UserInformationActivity;
+import com.example.tanvir.phonebookmanager.Database.DatabaseManager;
 import com.example.tanvir.phonebookmanager.R;
+import com.example.tanvir.phonebookmanager.models.ContactsInfo;
+
+import java.util.ArrayList;
 
 public class FavoriteFragment extends Fragment {
-    ListView listView;
+    ListView favListView;
+    DatabaseManager databaseManager;
+    TextView favEmptyTv;
+    SearchView favsearchView;
+    int contactId = 0;
 
     public interface OnFragmentInteractionListener {
 
@@ -25,20 +43,62 @@ public class FavoriteFragment extends Fragment {
     }
 
     @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false);
+        View view =  inflater.inflate(R.layout.fragment_favorite, container, false);
+        databaseManager=new DatabaseManager(getActivity());
 
-        /*Button button = view.findViewById(R.id.buttonId);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        favListView = view.findViewById(R.id.favListView);
+        favEmptyTv = view.findViewById(R.id.favEmptyTextView);
+        favsearchView = view.findViewById(R.id.favSearchView);
+
+        final ArrayList<ContactsInfo> contactsInfos = databaseManager.getContactByFavorite();
+        final ArrayList<ContactsInfo> contactsInfoss = databaseManager.getAllContacts();
+        ArrayList<String> favList = new ArrayList<>();
+
+        for (ContactsInfo contactsInfo:contactsInfos){
+            favList.add(contactsInfo.getContactName());
+        }
+
+        favListView.setEmptyView(favEmptyTv);//this function will be call when list is empty
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(),R.layout.listview_shape,R.id.textviewIdOnLv,favList);
+        favListView.setAdapter(arrayAdapter);
+
+        favsearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity().getApplication(), AddInformationsActivity.class);
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {//when user search on searchview this method will be called
+                arrayAdapter.getFilter().filter(s);//here, the search text will be filtered
+                return false;
+            }
+        });
+
+
+        favListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                contactId = contactsInfos.get(i).getId();
+                ContactsInfo contactsInfo = databaseManager.getContactsById(contactId);
+
+                String id = String.valueOf(contactId);
+                Intent intent = new Intent(getActivity(), UserInformationActivity.class);
+                intent.putExtra("id",id);
+                intent.putExtra("name",contactsInfo.getContactName());
+                intent.putExtra("phoneNum",contactsInfo.getContactNumber());
+                intent.putExtra("email",contactsInfo.getContactEmail());
+                intent.putExtra("description",contactsInfo.getContactDescription());
                 startActivity(intent);
             }
-        });*/
-        //return view;
+        });
+
+        return view;
     }
 }
