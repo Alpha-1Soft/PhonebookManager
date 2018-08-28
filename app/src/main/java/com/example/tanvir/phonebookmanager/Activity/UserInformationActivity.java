040a5901c7a407e7ac649a17385e2ca1363a7d07
hatanvir <tanvirhasan552@gmail.com>
2018-08-28 16:22:10 +0600

    call massage email feture added

--- app/src/main/java/com/example/tanvir/phonebookmanager/Activity/UserInformationActivity.java
+++ app/src/main/java/com/example/tanvir/phonebookmanager/Activity/UserInformationActivity.java
@@ -1,11 +1,17 @@
 package com.example.tanvir.phonebookmanager.Activity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -24,7 +30,9 @@ public class UserInformationActivity extends AppCompatActivity {
     public static final String IS_CHECKED = "chacked";
     DatabaseManager databaseManager;
     Button callBt,massageBt,extraCallBt;
-    TextView nameTv,phoneNumTv,emailTv;
+    TextView nameTv,phoneNumTv,emailTv,desTv;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1;
+
 
     CheckBox checkBox;
     SharedPreferences sharedPreferences;
@@ -37,11 +45,11 @@ public class UserInformationActivity extends AppCompatActivity {
 
         callBt = findViewById(R.id.callButton);
         massageBt = findViewById(R.id.massegeButton);
-        extraCallBt = findViewById(R.id.extraCallButton);
 
         nameTv = findViewById(R.id.contactNameTextView);
         phoneNumTv = findViewById(R.id.phoneNumberTv);
         emailTv = findViewById(R.id.emailTv);
+        desTv = findViewById(R.id.descriptionTv);
 
         checkBox = findViewById(R.id.favoriteCheckbox);
         //toggleButton=findViewById(R.id.toogle);
@@ -63,29 +71,82 @@ public class UserInformationActivity extends AppCompatActivity {
 
         nameTv.setText(name);
         phoneNumTv.setText(phoneNum);
-        emailTv.setText(email);
+
+        if (email.length()==0){
+            emailTv.setText("Empty eamil");
+        }
+        else{
+            emailTv.setText(email);
+        }
+        if (description.length()==0){
+            desTv.setText("Empty description");
+        }
+        else{
+            desTv.setText(description);
+        }
     }
 
 
 
     public void callMethod(View view) {
-        //implement here
+        checkForPhonePermissionJustCall();
     }
 
     public void massageMethod(View view) {
-        //implement here
-    }
-
-    public void extraCallMethod(View view) {
-        //implement here
+        String phoneNum = getIntent().getStringExtra("phoneNum");
+        Intent massageIntent = new Intent(Intent.ACTION_SENDTO);
+        massageIntent.setData(Uri.parse("smsto:"+phoneNum));
+        Intent massageChooser = Intent.createChooser(massageIntent,"Choose your application");
+        startActivity(massageChooser);
     }
 
     public void phoneNumberMethod(View view) {
-        //implement here
+        checkForPhonePermissionJustCall();
+    }
+    //checking phone permission here
+    private void checkForPhonePermissionJustCall() {
+        Intent intent = getIntent();
+        String phone = intent.getStringExtra("phoneNum");
+        if (phone.length() > 0) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                // Permission not yet granted. Use requestPermissions().
+                // Log.d(Tag, getString(R.string.permission_not_grante));
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE},
+                        MY_PERMISSIONS_REQUEST_CALL_PHONE);
+            } else {
+                Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone));
+                //Intent intentChooser = Intent.createChooser(callIntent,"Choose your application");
+                startActivity(callIntent);
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Enter number", Toast.LENGTH_SHORT).show();
+        }
     }
 
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == MY_PERMISSIONS_REQUEST_CALL_PHONE){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                checkForPhonePermissionJustCall();
+            }
+        }
+        else Toast.makeText(getApplicationContext(),"Permission denied",Toast.LENGTH_SHORT).show();
+    }
+
+
     public void sendMailMethod(View view) {
-        //implement here
+        String email = getIntent().getStringExtra("email");
+        if(email.length()!=0){
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+            emailIntent.setData(Uri.parse("mailto:"));
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+            Intent emailChoose = Intent.createChooser(emailIntent, "Choose your application");
+            startActivity(emailChoose);
+        }
+        else{
+            Toast.makeText(this, "Empty email", Toast.LENGTH_SHORT).show();
+        }
     }
 
     //edit button
@@ -94,6 +155,23 @@ public class UserInformationActivity extends AppCompatActivity {
         String id = getIntent().getStringExtra("id");// receiving id from contacts fragment
         intent.putExtra("id",id);//passing specific contact id for updating contact information
         startActivity(intent);
+    }
+
+    public void descriptionMethod(View view) {
+        String description = getIntent().getStringExtra("description");
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(description);
+
+        if(description.length()>0){
+            AlertDialog.Builder alBuilder = new AlertDialog.Builder(this);
+            alBuilder.setMessage(stringBuffer.toString());
+            alBuilder.setTitle("Contact description");
+            alBuilder.setCancelable(true);
+            alBuilder.show();
+        }
+        else{
+            Toast.makeText(this, "Empty description", Toast.LENGTH_SHORT).show();
+        }
     }
 
     //on backpressed thing
@@ -143,5 +221,6 @@ public class UserInformationActivity extends AppCompatActivity {
             }
         }
     }
+
 
 }
